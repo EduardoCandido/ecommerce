02f1d2e0cbc61d856bdf77ef39896d0657855a99
Eduardo Candido <eduardocandidopro@gmail.com>
2018-10-01 11:31:53 -0300

    Esqueceu a senha

--- index.php
+++ index.php
@@ -72,8 +72,14 @@ $app->get('/admin/users/create', function(){
 });
 
 $app->get('/admin/users/:iduser/delete', function($iduser){
-
 	User::verifyLogin();
+	$user = new User();
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
 });
 
 $app->get('/admin/users/:iduser', function($iduser){
@@ -82,8 +88,13 @@ $app->get('/admin/users/:iduser', function($iduser){
 
 	$user = new User();
 	
+	$user->get((int)$iduser);
+	
 	$page = new PageAdmin();
-	$page->setTpl("users-update");
+	
+	$page ->setTpl("users-update", array(
+        "user"=>$user->getValues()
+    ));
 	exit;
 });
 
@@ -91,13 +102,84 @@ $app->post('/admin/users/create', function(){//Recebe os dados do /admin/users/c
 
 	User::verifyLogin();
 
+	$user = new User();
+	
+	$_POST['inadmin'] = (isset($_POST['inadmin']))?1:0;
+
+	$_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, [
+
+		"cost"=>12
+
+	]);
+
+	
+	$user->setData($_POST);
+	
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+
 });
 
 $app->post('/admin/users/:iduser', function($iduser){
 	
 	User::verifyLogin();
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$_POST['inadmin'] = (isset($_POST['inadmin']))?1:0;
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+
+
 });
 
+$app->get('/admin/forgot',function()
+{
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl("forgot");	
+});
+
+$app->post('/admin/forgot',function()
+{
+	
+	$user = User::getForgot($_POST['email']);
+	header("Location: /admin/forgot/sent");
+	exit;
+});
+
+$app->get('/admin/forgot/sent',function(){
+	
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl("forgot-sent");
+});
+
+$app->get('/admin/forgot/reset',function()
+{
+	$user = User::validForgotDecrypt($_GET['code']);
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl("forgot-reset",array(
+		"name"=>$user['desperson'],
+		"code"=>$_GET['code']
+	));
+
+});
 
 
 
