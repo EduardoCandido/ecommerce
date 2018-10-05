57abd98e7e8dd8bd68aea95f70b7223d4c991c10
Eduardo Candido <eduardocandidopro@gmail.com>
2018-10-05 15:48:43 -0300

    Esqueci a Senha-Admin

--- index.php
+++ index.php
@@ -176,19 +176,23 @@ $app->get('/admin/forgot/reset',function()
 	]);
 	$page->setTpl("forgot-reset",array(
 		"name"=>$user['desperson'],
-		"code"=>$_SERVER['REMOTE_ADDR']
+		"code"=>$_GET['code']
 	));
 
 });
 
 $app->post('/admin/forgot/reset',function(){
 	$forgot = User::validForgotDecrypt($_POST['code']);
-	User::setForgotUsed($forgot["idrecovery"]);
+	User::setForgotUsed($forgot['idrecovery']);
 
 	$user = new User();
 	$user->get((int)$forgot['iduser']);
 
-	$user->setPassword($_POST['password']);
+	$password = password_hash($_POST['password'], PASSWORD_DEFAULT, [
+		"cost"=>12
+	]);
+
+	$user->setPassword($password);
 
 	$page = new PageAdmin([
 		"header"=>false,
